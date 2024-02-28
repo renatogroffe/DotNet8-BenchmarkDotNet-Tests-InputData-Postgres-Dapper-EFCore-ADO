667cbce8cac6ac44e7193a3d051c4f4e852a1d94
Renato Groffe <renato.groffe@yahoo.com.br>
2024-02-28 07:26:18 -0300

    Commit inicial
    Commit inicial

---
+++ BenchmarkingDapperEFCoreCRMPostgres/run-tests.ps1
@@ -0,0 +1,6 @@
+$env:NumeroContatosPorCompanhia = "2"
+$env:BaseEFCoreConnectionString = "Server=localhost;Port=5432;Database=basecrmef;User Id=postgres;Password=Postgres2024!"
+$env:BaseDapperConnectionString = "Server=localhost;Port=5432;Database=basecrmdapper;User Id=postgres;Password=Postgres2024!"
+$env:BaseDapperContribConnectionString = "Server=localhost;Port=5432;Database=basecrmdappercontrib;User Id=postgres;Password=Postgres2024!;"
+$env:BaseADOConnectionString = "Server=localhost;Port=5432;Database=basecrmado;User Id=postgres;Password=Postgres2024!"
+dotnet run --filter * -c Release
